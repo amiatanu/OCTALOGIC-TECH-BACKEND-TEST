284800e742b6d28dc86c999cde0ba58d1d3bd941
amiatanu <80941558+amiatanu@users.noreply.github.com>
2023-11-02 20:06:20 +0530

    express server setup

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 import { connection } from "./config/connection.js";
 import dotenv from "dotenv";
+import express from "express";
 dotenv.config();
+const app = express();
+//middleware setup
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+//server setup
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`server is running on ${process.env.SERVER_PORT}`);
+});
+
+//database connection
 connection();
